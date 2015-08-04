e02ee82668b3c36837b07dd84a51568d9d9b1e24
david <davidslee101@gmail.com>
2015-08-04 02:03:27 +0000

    finished nomster

--- db/migrate/20150804013544_add_picture_to_photo.rb
+++ db/migrate/20150804013544_add_picture_to_photo.rb
@@ -1,5 +1,5 @@
 class AddPictureToPhoto < ActiveRecord::Migration
   def change
-    add_column :photos, :Picture, :string
+    add_column :photos, :picture, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150803215452) do
+ActiveRecord::Schema.define(version: 20150804013544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,11 +34,8 @@ ActiveRecord::Schema.define(version: 20150803215452) do
     t.integer  "place_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "Picture"
+    t.string   "picture"
   end
-
-  add_index "photos", ["place_id"], name: "index_photos_on_place_id", using: :btree
-  add_index "photos", ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id", using: :btree
 
   create_table "places", force: true do |t|
     t.string   "name"
@@ -66,8 +63,6 @@ ActiveRecord::Schema.define(version: 20150803215452) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "avatar"
-    t.string   "picture"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
